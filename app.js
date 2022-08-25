e9c4963408b986d090b5db900f322077ddf31b3c
Madeline <needhelpwithrift@gmail.com>
2022-08-25 15:02:57 +0100

    SOME professionalism is needed, otherwise this'll look more amateur than it already does

--- app.js
+++ app.js
@@ -8,9 +8,11 @@
 var express = require("express");
 var app = express();
 const allowedCodes = [
-    'sampleCode'
+    'sampleCode',
+    'anotherSampleCode',
+    'verified',
 ]
-const doVerification = false;
+const doVerification = true;
 app.use((req, res, next) => {
     console.log(`${req.method} request made to "${req.baseUrl + req.path}" ${req.query ? `using code "${req.query.code}"` : `without a code`} at ${new Date().toLocaleTimeString()}.`)
     next();
@@ -18,19 +20,18 @@ app.use((req, res, next) => {
 
 app.get('/verify', (req, res) => {
     try {
+        if (req.query.code == "error") throw new Error("Sample error thrown.")
         if (!doVerification) return res.status(200).send(`${req.query.code}verified`)
-        if (allowedCodes.includes(req.query.code)) {
-            console.log(`physics mod just verified using code ${req.query.code}`)
+        if (allowedCodes.includes(req.query.code) && req.query.code) {
             return res.status(200).send(`${req.query.code}verified`) // no space bcs String.contains("abcd " + "verified") returns false in java
         }
-        console.log(`someone just failed to verify using code ${req.query.code}`)
-        return res.status(200).send(`invalid code! valid codes are: [ ${allowedCodes.toString().replace(",", ", ")} ]`) // i don't know why but it always expects 200?? shitty code
+        return res.status(200).send(`Invalid code! Valid codes are ${allowedCodes.toString().replace(/,([^,]*)$/, ' and $1').replace(/,/g, ', ')}.`) // i don't know why but it always expects 200?? shitty code
     } catch(e) {
-        console.log(`failed to verify???? (${e})`) // this should never, EVER happen
-        return res.status(200).send(e)
+        console.log(`An error occured while verifying. (${e})`) // this should never, EVER happen
+        return res.status(200).send(`${e}`)
     }
 })
 
 app.listen(4567, () => {
-    console.log(`listening on port 4567`)
+    console.log(`Listening on port 4567`)
 })
